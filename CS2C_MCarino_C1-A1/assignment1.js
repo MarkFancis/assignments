04045b0d96832543f3b41d7694c2f3fa815b011c
MarkFancis <02alter.19@gmail.com>
2024-09-24 22:34:24 +0800

    Add files via upload

---
+++ CS2C_MCarino_C1-A1/assignment1.js
@@ -0,0 +1,20 @@
+let fname = "mark francis "
+let mname = "orpilla "
+let lname = "carino "
+let fullname = (fname + mname + lname)
+let Fullname = fullname.toUpperCase()
+let birthDay = "february 19 2005"
+let birthPlace = "langlangca primero, candon city, ilocos sur, philipines, 2710"
+let BirthPlace = birthPlace.toUpperCase()
+let address = "langlangca primero, candon city, ilocos sur, philipines, 2710"
+let Address = address.toUpperCase()
+let course = "bachelor of science in computer science"
+let year = "second year"
+let DreamJob = "Software Engineer"
+
+
+
+
+console.log(Fullname + "was born",birthDay + " at", BirthPlace + " and currently living at", Address + ".", fullname + "is taking up", course
+    + "and dreams to be", DreamJob + "after graduation"
+ )
---
+++ CS2C_MCarino_C1-A1/assignment2.js
@@ -0,0 +1,27 @@
+let a = 25 
+let b = 20
+let c = 12
+let d = "15"
+const e = 15
+
+d = parseInt(d)
+
+console.log(a + b + c + d + e)
+console.log(d > e)
+console.log(d < e)
+console.log(d >= e)
+console.log(d <= e)
+console.log(d === e)
+
+let dif = (a - b - c - d)
+let prod = (a * c)
+let quot = (prod / e)
+let val = (c * 3 + e)
+
+
+console.log(dif)
+console.log(quot)
+console.log(e ** d)
+console.log(val)
+
+
